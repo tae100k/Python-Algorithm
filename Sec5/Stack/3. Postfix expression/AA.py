a4e0eda44869a5b2ddfdd7897b2a0e0b1787f323
tae100k <78027252+tae100k@users.noreply.github.com>
2021-07-24 04:04:08 +0900

    2021 0724 0404

--- Sec5/Stack/3. Postfix expression/AA.py
+++ Sec5/Stack/3. Postfix expression/AA.py
@@ -1,14 +1,9 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-n = int(input())
-ath = [list(map(int, input().split())) for _ in range(n)]
+exp = list(input())
+temp = []
 res = []
 
-for a in ath:
-    if any(t[0] > a[0] and t[1] > a[1] for t in ath):
-        continue
-    else:
-        res.append(a)
-
-print(len(res))
+while exp:
+    
