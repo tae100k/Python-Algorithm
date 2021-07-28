b90edfe1dc75f325a4d87781648b460c4f3c2ec7
tae100k <tkim3@oswego.edu>
2021-07-29 01:13:33 +0900

    2021 0729 0113

--- Sec6/DFS/8. Guessing permutation/AA.py
+++ Sec6/DFS/8. Guessing permutation/AA.py
@@ -1,23 +1,27 @@
-#inpt = sys.stdin.readline하면 더 빠르게 파일을 읽음
-
+#꼭 다시 풀어보기
+# 특히 파스칼 삼각형 부분 스스로 쓸 수 있도록 하기!
 
 import sys
-#sys.stdin = open("input.txt", "r")
+sys.stdin = open("input.txt", "r")
 
-def DFS(v):
-    global cnt
-    if v == m:
-         for c in ch:
-             print(c, end = '')
-         print()
-         cnt +=1
+def DFS(v, tot):
+    if v == n and tot == f:
+        for x in res:
+            print(x, end = '')
+        sys.exit(1)
     else:
         for k in range(1, n+1):
-            ch[v] = k
-            DFS(v+1)
+            if ch[k] == 0:
+                ch[k]= 1
+                res[v] = k
+                DFS(v+1, tot+(bi[v] * res[v]))
+                ch[k]= 0
 
-n, m = map(int, input().split())
-ch = [0] * m
-cnt = 0
-DFS(0)
-print(cnt)
+n, f = map(int, input().split())
+res = [0] * n
+ch = [0] * (n+1)
+bi = [1] * n
+for i in range(1, n):
+    bi[i] = bi[i-1] * (n-i) / i
+    
+DFS(0, 0) 
