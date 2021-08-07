9bffffae21eff749be66c8e34b18c49e7c63b9a6
tae100k <tkim3@oswego.edu>
2021-08-07 23:39:41 +0900

    2021 0807 -1

--- Sec7/DFS/06.Alpha Code/AA.py
+++ Sec7/DFS/06.Alpha Code/AA.py
@@ -1,10 +1,25 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def DFS(i):
+def DFS(i, prev):
+    global cnt
     if i == size:
         print(str)
+        cnt +=1
+    else:
+        if code[i] in [0,1,2]:
+            str.append(chr(code[i]))
+            DFS(i+1)
+            str.pop()
+            
+            DFS(i+1, code[i])
+        else:
+            str.append(chr(code[i]))
+            DFS(i+1)
+            
 
 code= int(input())
 size = len(code)
+cnt = 0
+str = []
 DFS()
