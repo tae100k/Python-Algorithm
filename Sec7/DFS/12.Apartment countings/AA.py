8b7257f2b87a5900cb9c65fc8866a12030d9198a
tae100k <tkim3@oswego.edu>
2021-08-24 18:27:49 +0900

    2021 0824 -2

--- Sec7/DFS/12.Apartment countings/AA.py
+++ Sec7/DFS/12.Apartment countings/AA.py
@@ -1,35 +1,34 @@
+# 문제에서 띄어쓰기가 없는데 split()을 했음 -> 문제를 제대로 읽지 않음
+# 문제를 제대로 읽지 않음 오름차순이라는 말을 못챙기함
+
+
 import sys
 sys.stdin = open("input.txt", "r")
 
-def DFS(x, y):
-    global cnt
-    if x == endx and y == endy:
-        cnt +=1
-    else:
-        for k in range(4):
-            xx = x + dx[k]
-            yy = y + dy[k]
-            if 0<= xx<n and 0<= yy <n and board[x][y] < board[xx][yy]:
-                DFS(xx, yy)
-        
+def DFS(x,y):
+    global apt
+    apt +=1
+    board[x][y] = 0
+    for k in range(4):
+        xx = x + dx[k]
+        yy = y + dy[k]
+        if 0<= xx < n and 0<= yy < n and board[xx][yy] == 1:
+            DFS(xx,yy)
 
 n = int(input())
-board = [list(map(int,input().split())) for _ in range(n)]
-st = 2147000000
-end = -2147000000
+board = [list(map(int, input())) for _ in range(n)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-cnt = 0
+answers = []
+
 for i in range(n):
     for j in range(n):
-        if st > board[i][j]:
-            st = board[i][j]
-            stx = i
-            sty = j
-        if end < board[i][j]:
-            end = board[i][j]
-            endx = i
-            endy = j
-DFS(stx,sty)
-print(cnt)
-            
+        if board[i][j] == 1:
+            apt = 0
+            DFS(i,j)
+            answers.append(apt)
+
+print(len(answers))
+answers = sorted(answers)
+for answer in answers:
+    print(answer)
