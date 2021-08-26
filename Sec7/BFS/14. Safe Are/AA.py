94f55a0612e5219ecc00503a0ad07f4e291ceb5a
tae100k <tkim3@oswego.edu>
2021-08-26 15:05:43 +0900

    2021 0826

--- Sec7/BFS/14. Safe Are/AA.py
+++ Sec7/BFS/14. Safe Are/AA.py
@@ -2,27 +2,40 @@ import sys
 from collections import deque
 #sys.stdin = open("input.txt", "r")
 
-n = int(input())
-board = [ list(map(int, input().split())) for _ in range(n)]
-dq = deque()
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-dy = [0, 1, 1,1,0, -1, -1, -1]
-answer = []
 
+n = int(input())
+ch = [[0] * n for _ in range(n)]
+board = [list(map(int, input().split())) for _ in range(n)]
+dq = deque()
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+big = 0
+cnt = 0
+answer = []
 for i in range(n):
     for j in range(n):
-        if board[i][j] == 1:
-            dq.append((i,j))
-            board[i][j] = 0
-            cnt = 1
-            while dq:
-                cur = dq.popleft()
-                for k in range(8):
-                    xx = cur[0] + dx[k]
-                    yy = cur[1] + dy[k]
-                    if 0<=xx<n and 0<=yy<n and board[xx][yy] == 1:
-                        dq.append((xx,yy))
-                        board[xx][yy] = 0
-                        
-print(len(answer))
+       if big < board[i][j]:
+           big = board[i][j]
+
+for h in range(0, big+1):
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] >= h and ch[i][j] == 0:
+                dq.append((i,j))
+                ch[i][j] = 1
+                cnt +=1
+                while dq:
+                    cur = dq.popleft()
+                    for f in range(4):
+                            xx = cur[0] + dx[f]
+                            yy = cur[1] + dy[f]
+                            if 0<=xx<n and 0<=yy<n and ch[xx][yy] == 0 and board[xx][yy] >= h:
+                                dq.append((xx,yy))
+                                ch[xx][yy] = 1
+                
+    answer.append(cnt)
+    cnt = 0
+    ch = [[0] * n for _ in range(n)]
+print(max(answer))
+                    
                 
