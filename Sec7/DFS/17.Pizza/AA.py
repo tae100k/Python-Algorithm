8892ac62f3c78e56d165f2a60559fb2a2ca4694d
tae100k <tkim3@oswego.edu>
2021-08-30 21:02:36 +0900

    2021 0830

--- Sec7/DFS/17.Pizza/AA.py
+++ Sec7/DFS/17.Pizza/AA.py
@@ -1,17 +1,45 @@
+#조합을 만들어두고 사용하지 않음
+
 import sys
-sys.stdin = open("input.txt", "r")
+#sys.stdin = open("input.txt", "r")
 
+def DFS(L, s):
+    global res
+    if L == m:
+        one = 0
+        for h in hs:
+            hx = h[0]
+            hy = h[1]
+            sma = 2147000000
+            for c in comb:
+                px = pz[c][0]
+                py = pz[c][1]
+                dis = abs(hx - px) + abs(hy-py)
+                if sma > dis:
+                    sma = dis                
+            one += sma
+        if one < res:
+            res = one
+    else:
+        for k in range(s, len(pz)):
+            comb[L] = k
+            DFS(L+1, k+1)
+            
 
-def DFS(L, p):
-    if 
-
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-pz = 0
-for i in range(n):
-    for j in range(m):
-        if board[i][j] == 2:
-            pz += 1
+pz = []
+hs = []
+sma = 2147000000
+res = 2147000000
+comb = [0] * m
 
-arr = [0] * m
-DFS(0)
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            hs.append((i,j))
+        elif board[i][j] == 2:
+            pz.append((i,j))
+
+DFS(0,0)
+print(res)
